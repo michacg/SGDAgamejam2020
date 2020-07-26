bc097a8b4377277349f1e5b6f19555d24e88ae22
michacg <michacg2@uci.edu>
2020-07-26 16:15:00 -0700

    panel 1

---
+++ GameJam2020/Assets/Scripts/Play.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+using TMPro;
+
+public class Play : MonoBehaviour
+{
+
+    [SerializeField] GameObject fadeInObj;
+    [SerializeField] GameObject button;
+
+    public void StartChapter()
+    {
+        StartCoroutine(Fade(true));
+        StartCoroutine(Fade(false));
+    }
+
+    private IEnumerator Fade(bool fadeIn)
+    {
+
+        if (fadeIn)
+        {
+            for (float i = 0; i <= 0.48; i += Time.deltaTime)
+            {
+                fadeInObj.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+        }
+        else
+        {
+            for (float i = 1; i >= 0; i -= Time.deltaTime)
+            {
+                button.GetComponent<Image>().color = new Color(1, 1, 1, i);
+                button.SetActive(false);
+                yield return null;
+            }
+        }
+
+    }
+}
