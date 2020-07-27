16b464cbe90e1e38198e02377ee1257f87c76072
Michaela <michaelacfaith@gmail.com>
2020-07-27 00:01:50 -0700

    fixed hand thingy sorry

--- GameJam2020/Assets/Scripts/MoveHand.cs
+++ GameJam2020/Assets/Scripts/MoveHand.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class MoveHand : MonoBehaviour
 {
     [SerializeField] private float handSpeed = 2f;
-/*    [SerializeField] private float maxZ = 34.949f;
-    [SerializeField] private float minZ = -15.405f;*/
+    [SerializeField] private float maxZ = 34.949f;
+    [SerializeField] private float minZ = -15.405f;
     [SerializeField] private float rotZ;
     
 
@@ -17,7 +17,19 @@ public class MoveHand : MonoBehaviour
 
     void Update()
     {
-        rotZ -= Input.GetAxis("Mouse X");
-        transform.Rotate(new Vector3(0, 0, rotZ) * Time.deltaTime * handSpeed);
+        if (Input.GetAxis("Mouse X") == 0)
+            transform.Rotate(new Vector3(0, 0, rotZ) * Time.deltaTime * 0);
+
+        else
+        {
+            rotZ -= Input.GetAxis("Mouse X");
+            rotZ %= 360f;
+
+            if (rotZ > maxZ) { rotZ = maxZ; }
+            if (rotZ < minZ) { rotZ = minZ; }
+
+            transform.Rotate(new Vector3(0, 0, rotZ) * Time.deltaTime * handSpeed);
+        }
+
     }
 }
